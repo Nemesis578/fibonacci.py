8c8428572aa8280a64b2014c9639f62d49cf3d8a
Nemesis578 <66907355+Nemesis578@users.noreply.github.com>
2020-07-10 18:49:30 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,16 @@
+# Deleting different dictionary elements
+
+dict = {
+    "Name": "Lin Dan",
+    "Nationality" : "Chinese",
+    "Date of Birth": "14 October, 1983",
+    "Age":36, 
+    "Sports": "Badminton",
+    "Sponsorship": "Yonex",
+    "Handedness" : "Left",
+    "Coach": "Xia Xianze",
+}
+
+dict.pop("Coach")
+
+print(dict)
